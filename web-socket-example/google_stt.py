3c22a389e09594fe3f4410c9fdc71fdb0e821839
Pablo Grill <pablo.grill@xmartlabs.com>
2022-01-19 17:47:54 -0300

    Buffering audio chunks.

--- web-socket-example/google_stt.py
+++ web-socket-example/google_stt.py
@@ -1,3 +1,5 @@
+import logging
+
 from google.cloud import speech
 
 
@@ -7,15 +9,17 @@ class GoogleSTT:
         self.client = speech.SpeechClient()
 
     def speech_to_text(self, content: bytes) -> str:
-        audio = speech.RecognitionAudio(content=content)
-        config = speech.RecognitionConfig(
-            encoding=speech.RecognitionConfig.AudioEncoding.ENCODING_UNSPECIFIED,
-            sample_rate_hertz=16000,
-            language_code="en-US",
-        )
-        # Detects speech in the audio file
-        response = self.client.recognize(config=config, audio=audio)
-        if response and response.results:
-            # TODO: Improve dealing with multiple results
-            return response.results[0].alternatives[0].transcript
-        return ""
+        try:
+            audio = speech.RecognitionAudio(content=content)
+            config = speech.RecognitionConfig(
+                encoding=speech.RecognitionConfig.AudioEncoding.ENCODING_UNSPECIFIED,
+                language_code="en-US",
+            )
+            # Detects speech in the audio file
+            response = self.client.recognize(config=config, audio=audio)
+            if response and response.results:
+                # TODO: Improve dealing with multiple results
+                return response.results[0].alternatives[0].transcript
+            return ""
+        except Exception as e:
+            logging.error(e)
--- web-socket-example/main.py
+++ web-socket-example/main.py
@@ -9,7 +9,12 @@ gstt = GoogleSTT()
 @app.websocket("/ws")
 async def websocket_endpoint(websocket: WebSocket):
     await websocket.accept()
+    stream_bytes = None
     while True:
         data = await websocket.receive_bytes()
-        transcription = gstt.speech_to_text(data)
+        if not bytes:
+            stream_bytes = data
+        else:
+            stream_bytes += data
+        transcription = gstt.speech_to_text(stream_bytes)
         await websocket.send_text(f"Message text was: {transcription}")
